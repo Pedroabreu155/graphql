71b278dc6f7f03d695348718575206e2a6dc647c
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-10-25 14:39:55 -0300

    created merge into resolvers

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,1 +1,9 @@
-export default {};
+import { fileLoader, mergeResolvers } from 'merge-graphql-schemas';
+import path from 'path';
+
+const resolversArray = fileLoader(
+  path.join(__dirname, 'modules', '**', 'resolvers.js')
+);
+const resolvers = mergeResolvers(resolversArray);
+
+export { resolvers };
--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -2,6 +2,6 @@ import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
 import path from 'path';
 
 const typesArray = fileLoader(path.join(__dirname, 'modules', '**', '*.gql'));
-const typeRefs = mergeTypes(typesArray);
+const typeDefs = mergeTypes(typesArray);
 
-export { typeRefs };
+export { typeDefs };
